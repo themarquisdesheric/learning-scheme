f10681126fa228d98bc48a062f3f9dd5b3c61732
Yuval Allweil <themarquisdesheric@gmail.com>
2022-09-04 21:50:53 -0700

    insertR and insertL functions

--- scratchpad.scm
+++ scratchpad.scm
@@ -37,7 +37,37 @@
 ;               (list `g `h `i)))
 ;Value: (a d g)
 
-(firsts (list (list `a `b `c)
-              (list (list `nested `list))
-              (list `d `e `f)))
+; (firsts (list (list `a `b `c)
+;               (list (list `nested `list))
+;               (list `d `e `f)))
 ;Value: (a (nested list) d)
+
+
+; given the parameters (new old lat) return a list of atoms with new inserted to the right of the first occurrence of old, followed by the rest of the list
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+              ((eq? (car lat) old) (cons (car lat)
+                                         (cons new
+                                               (cdr lat))))
+              (else (cons (car lat)
+                          (insertR new old (cdr lat)))))))))
+
+; (insertR `practice `more (list `give `me `some `more `please))
+;Value: (give me some more practice please)
+
+
+; given the parameters (new old lat) return a list of atoms with new inserted to the left of the first occurrence of old, followed by the rest of the list
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+              ((eq? (car lat) old) (cons new lat))
+              (else (cons (car lat)
+                          (insertL new old (cdr lat)))))))))
+
+; (insertL `spicy `food (list `give `me `some `food))
+;Value: (give me some spicy food)
