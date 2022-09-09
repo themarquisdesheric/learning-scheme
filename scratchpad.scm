b3ce9329678315caf3ad727c52ae8908e914bf56
Yuval Allweil <themarquisdesheric@gmail.com>
2022-09-08 19:01:23 -0700

    refactor subst2 to use the or operator

--- scratchpad.scm
+++ scratchpad.scm
@@ -93,8 +93,8 @@
     (cond
       ((null? lat) (quote ()))
       (else (cond
-              ((eq? (car lat) o1) (cons new (cdr lat)))
-              ((eq? (car lat) o2) (cons new (cdr lat)))
+              ((or (eq? (car lat) o1) (eq? (car lat) o2)
+               (cons new (cdr lat))))
               (else (cons (car lat)
                           (subst2 new o1 o2 (cdr lat)))))))))
 
