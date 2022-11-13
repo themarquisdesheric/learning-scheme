5a083c393f028c439ce6d13397a07cebe912a2f2
Yuval Allweil <themarquisdesheric@gmail.com>
2022-11-12 20:41:56 -0800

    eqlist? function

--- scratchpad.scm
+++ scratchpad.scm
@@ -471,6 +471,7 @@
       (else (or (member* a (car l)) (member* a (cdr l)))))))
 
 (member* `hey (list `now (list `you `hey `now)))
+;Value: #t
 
 ; write the function `leftmost` which returns the leftmost atom in a non-empty list of S-expressions that does not contain the empty list
 (define leftmost
@@ -480,3 +481,21 @@
       (else (leftmost (car l))))))
 
 (leftmost (list (list (list `here)) (list `not `this `or `this)))
+;Value: here
+
+; write the function `eqlist?` which determines if two lists are equal (including nested lists). Use the function `eqan?`
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+      ((and (null? l1) (null? l2)) #t)
+      ((or (null? l1) (null? l2)) #f)
+      ((and (atom? (car l1)) (atom? (car l2)))
+        (and (eqan? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2))))
+      ((or (atom? (car l1)) (atom? (car l2))) #f)
+      (else (and (eqlist? (car l1) (car l2))
+                 (eqlist? (cdr l1) (cdr l2)))))))
+
+; (eqlist? (list `hey) (list `hey `hey))
+;Value: #f
+(eqlist? (list (list `hey `now)) (list (list `hey `now)))
+;Value: #t
